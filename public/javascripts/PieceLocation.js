8e131cb387f6c9aa353d9dce606ab1e918640cc8
jiayi <jiayizhan2005@gmail.com>
2024-11-28 22:21:13 +0100

    Implement game logic

---
+++ public/javascripts/PieceLocation.js
@@ -0,0 +1,11 @@
+class PieceLocation {
+  draggable = null;
+  
+  constructor(ref, top, left, height, width) {
+    this.ref = ref;
+    this.top = top;
+    this.left = left;
+    this.height = height;
+    this.width = width;
+  }
+}
--- public/javascripts/play.js
+++ public/javascripts/play.js
@@ -2,6 +2,7 @@
  * @typedef {import("jquery")} $
  * @typedef {import("gsap")} gsap
  * @typedef {import("gsap/Draggable")} Draggable
+ * @typedef {import("./PieceLocation")} PieceLocation
  */
 
 gsap.registerPlugin(Draggable);
@@ -15,6 +16,10 @@ var puzzleImage = {
   rowCount: 4,
   colCount: 4,
 };
+var draggableCollection = Array(puzzleImage.rowCount)
+  .fill()
+  .map((_) => Array(puzzleImage.colCount).fill());
+var overlappedCell = null;
 
 initScripts();
 
@@ -26,9 +31,10 @@ function initScripts() {
 function splitPieces() {
   $(function () {
     puzzleImage.ref = $("#puzzle-image");
+    const { colCount, rowCount } = puzzleImage;
 
-    const pieceWidth = puzzleImage.ref.width() / puzzleImage.colCount;
-    const pieceHeight = puzzleImage.ref.height() / puzzleImage.rowCount;
+    const pieceWidth = puzzleImage.ref.outerWidth() / colCount;
+    const pieceHeight = puzzleImage.ref.outerHeight() / rowCount;
 
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
@@ -36,7 +42,17 @@ function splitPieces() {
     canvas.width = pieceWidth;
     canvas.height = pieceHeight;
 
-    console.log(pieceHeight, pieceWidth);
+    const randomSequence = Array.from(
+      { length: colCount * rowCount },
+      (_, i) => i + 1
+    );
+    for (let i = randomSequence.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [randomSequence[i], randomSequence[j]] = [
+        randomSequence[j],
+        randomSequence[i],
+      ];
+    }
 
     for (let row = 0; row < puzzleImage.rowCount; ++row) {
       for (let col = 0; col < puzzleImage.colCount; ++col) {
@@ -57,16 +73,129 @@ function splitPieces() {
         );
 
         const piece = $(new Image())
-          .attr("src", canvas.toDataURL())
+          .attr({ src: canvas.toDataURL(), row: row, col: col })
           .appendTo($(".pieces-container"))
           .css({
             cursor: "pointer",
+            width: `calc(min(65vw, 40vh) / ${puzzleImage.colCount})`,
+            height: `calc(min(65vw, 40vh) / ${puzzleImage.colCount})`,
+            order: `${randomSequence[row * rowCount + col]}`,
           });
 
-        // Draggable.create(piece);
+        draggableCollection[row][col] = Draggable.create(piece, {
+          bounds: "body",
+          onDrag: function () {
+            if ($(this.target).hasClass("down")) {
+              const id = $(this.target).attr("cell");
+              $(this.target).attr("cell", "").removeClass("down");
+              $(`#${id}`).removeClass("occupied");
+            }
+            movePiece(this.target);
+          },
+          onDragEnd: function () {
+            pieceDown(this.target, row, col);
+          },
+        });
       }
     }
   });
 }
 
-function createBoard() {}
+function createBoard() {
+  $(function () {
+    $(".grid").css(
+      "grid-template-columns",
+      `repeat(${puzzleImage.colCount}, 1fr)`
+    );
+
+    for (let row = 0; row < puzzleImage.rowCount; ++row)
+      for (let col = 0; col < puzzleImage.colCount; ++col)
+        $("<div>", { class: "cell" })
+          .appendTo($(".grid"))
+          .attr({
+            row: row,
+            col: col,
+            id: `cell${row * puzzleImage.rowCount + col}`,
+          });
+  });
+}
+
+function movePiece(piece) {
+  checkOverlap(piece);
+}
+
+function pieceDown(piece, row, col) {
+  if (!overlappedCell) return;
+  console.log("down");
+
+  $(overlappedCell).removeClass("overlap").addClass("occupied");
+
+  $(piece)
+    .offset({
+      top: $(overlappedCell).offset().top,
+      left: $(overlappedCell).offset().left,
+    })
+    .addClass("down")
+    .attr("cell", $(overlappedCell).attr("id"));
+}
+
+function checkOverlap(piece) {
+  let overlapped = null;
+  const trunc = (n) => n << 0; // remove the decimals from n
+
+  $(".cell").each(function () {
+    if ($(this).hasClass("occupied")) return true;
+
+    const cellWidth = trunc($(this).outerWidth()),
+      cellHeight = trunc($(this).outerHeight()),
+      cellX1 = trunc($(this).offset().left),
+      cellX2 = trunc(cellX1 + cellWidth),
+      cellY1 = trunc($(this).offset().top),
+      cellY2 = trunc(cellY1 + cellHeight);
+
+    const floatingWidth = trunc($(piece).outerWidth()),
+      floatingHeight = trunc($(piece).outerHeight()),
+      floatingX1 = trunc($(piece).offset().left),
+      floatingX2 = trunc(floatingX1 + floatingWidth),
+      floatingY1 = trunc($(piece).offset().top),
+      floatingY2 = trunc(floatingY1 + floatingHeight);
+
+    const minOverlap = 0.6; // range (0, 1)
+
+    if (
+      // top-left overlap
+      (floatingX2 >= cellX2 - cellWidth * (1 - minOverlap) &&
+        floatingX2 <= cellX2 &&
+        floatingY2 >= cellY2 - cellHeight * (1 - minOverlap) &&
+        floatingY2 <= cellY2) ||
+      // top-right overlap
+      (floatingX1 <= cellX1 + cellWidth * (1 - minOverlap) &&
+        floatingX1 >= cellX1 &&
+        floatingY2 >= cellY2 - cellHeight * (1 - minOverlap) &&
+        floatingY2 <= cellY2) ||
+      // bottom-left overlap
+      (floatingX2 >= cellX2 - cellWidth * (1 - minOverlap) &&
+        floatingX2 <= cellX2 &&
+        floatingY1 <= cellY1 + cellHeight * (1 - minOverlap) &&
+        floatingY1 >= cellY1) ||
+      // bottom-right overlap
+      (floatingX1 <= cellX1 + cellWidth * (1 - minOverlap) &&
+        floatingX1 >= cellX1 &&
+        floatingY1 <= cellY1 + cellHeight * (1 - minOverlap) &&
+        floatingY1 >= cellY1)
+    )
+      overlapped = this;
+
+    if (overlapped !== null) {
+      $(overlappedCell).removeClass("overlap");
+      $(overlapped).addClass("overlap");
+      overlappedCell = overlapped;
+      return false;
+    }
+  });
+
+  if (overlapped === null) {
+    $(overlappedCell).removeClass("overlap");
+    overlappedCell = null;
+  }
+}
